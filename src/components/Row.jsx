298cdb5477339895c9ac2280e23adb85beb5fe0a
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-10-02 10:26:55 +0600

    ui compleate

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -7,7 +7,7 @@ import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL,rowID }) => {
   const [movies, setMovies] = useState([]);
   const [like, setLike] = useState(false);
 
@@ -19,12 +19,21 @@ const Row = ({ title, fetchURL }) => {
 
   console.log(movies);
 
+  const sliderLeft = () =>{
+    var slider = document.getElementById('slider'+rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  }
+  const sliderRight = () =>{
+    var slider = document.getElementById('slider'+rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
+
   return (
     <div>
       <h2 className="text-white p-4 font-bold md:text-xl">{title}</h2>
       <div className="relative flex items-center group">
-        <MdChevronLeft size={40} className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"/>
-        <div id={"slider"} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+        <MdChevronLeft onClick={sliderLeft} size={40} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"/>
+        <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
           {movies.map((item, id) => (
             <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 ">
             <img
@@ -47,7 +56,7 @@ const Row = ({ title, fetchURL }) => {
           </div>
           ))}
         </div>
-        <MdChevronRight size={40} className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"/>
+        <MdChevronRight onClick={sliderRight} size={40} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"/>
       </div>
       
     </div>
